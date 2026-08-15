5213a505ee0a264c2ed3178b72358db00ae05be7
Mad0 <jose.antonio.pepe@icloud.com>
2026-08-15 13:24:27 -0600

    Add classes, get, set

--- src/lox/LoxFunction.java
+++ src/lox/LoxFunction.java
@@ -7,9 +7,11 @@ class LoxFunction implements LoxCallable {
     private final List<Token> parameters;
     private final List<Stmt> body;
     private final Environment closure;
+    private final boolean isInitializer;
 
     // function declaration
-    LoxFunction(Stmt.Function declaration, Environment closure) {
+    LoxFunction(Stmt.Function declaration, Environment closure, boolean isInitializer) {
+        this.isInitializer = isInitializer;
         this.name = declaration.name.lexeme;
         this.parameters = declaration.params;
         this.body = declaration.body;
@@ -17,11 +19,28 @@ class LoxFunction implements LoxCallable {
     }
 
     // function expression (anonymous function, lambda)
-    LoxFunction(Expr.Function declaration, Environment closure) {
+    LoxFunction(Expr.Function declaration, Environment closure, boolean isInitializer) {
+        this.isInitializer = isInitializer;
         this.name = null;
         this.parameters = declaration.params;
         this.body = declaration.body;
         this.closure = closure;
+    }
+
+    // function constructor for function bind
+    private LoxFunction(String name, List<Token> parameters, List<Stmt> body, Environment closure, boolean isInitializer) {
+        this.isInitializer = isInitializer;
+        this.name = name;
+        this.parameters = parameters;
+        this.body = body;
+        this.closure = closure;
+    }
+
+
+    LoxFunction bind(LoxInstance instance) {
+        Environment environment = new Environment(closure);
+        environment.define("this", instance);
+        return new LoxFunction(name, parameters, body, environment, isInitializer);
     }
 
     @Override
@@ -35,8 +54,12 @@ class LoxFunction implements LoxCallable {
         try {
             interpreter.executeBlock(body, environment);
         } catch (Return returnValue) {
+            if (isInitializer) return closure.getAt(0, "this");
+            
             return returnValue.value;
         }
+
+        if (isInitializer) return closure.getAt(0, "this");
         return null;
     }
 
